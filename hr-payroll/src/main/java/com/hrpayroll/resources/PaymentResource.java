8693dde69e41f2a5c99d1f6f05796e12bfd49ba3
LuizFernandesOliveira <luizfernandesoliveiraoficial@gmail.com>
2021-12-08 20:14:30 -0300

    hystrix tolerance

--- hr-payroll/src/main/java/com/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.hrpayroll.resources;
 
 import com.hrpayroll.entities.Payment;
 import com.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentResource {
   private PaymentService service;
 
+  @HystrixCommand(fallbackMethod = "getPaymentAlternative")
   @GetMapping(value = "/{workerId}/days/{days}")
   public ResponseEntity<Payment> getPayment(
     @PathVariable Long workerId,
     @PathVariable Integer days) {
     Payment payment = service.getPayment(workerId, days);
+    return ResponseEntity.ok(payment);
+  }
+
+  public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+    Payment payment = Payment.builder()
+      .name("Test")
+      .days(days)
+      .dailyIncome(300.0)
+      .build();
     return ResponseEntity.ok(payment);
   }
 }
