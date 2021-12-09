9860382be7c3e0e93db4a64aff055c7e676f4cc0
LuizFernandesOliveira <luizfernandesoliveiraoficial@gmail.com>
2021-12-08 21:45:49 -0300

    hr-user user many to many association role

---
+++ hr-user/src/main/java/com/hruser/entities/Role.java
@@ -0,0 +1,28 @@
+package com.hruser.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String roleName;
+}
